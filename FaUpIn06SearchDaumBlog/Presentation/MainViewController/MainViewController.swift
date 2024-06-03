bbc7b7069cbd08f179941911bc2c451f2cb06f81
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-06-03 22:12:13 +0900

    process observable data

--- FaUpIn06SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
+++ FaUpIn06SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
@@ -29,12 +29,89 @@ class MainViewController: UIViewController {
     }
     
     private func bind() {
+        let blogResult = searchBar.shouldLoadResult
+            .flatMapLatest { query in
+                SearchBlogNetwork().searchBlog(query: query)
+            }
+            .share()
+        
+        let blogValue = blogResult
+            .compactMap { data -> DKBlog? in
+                guard case .success(let value) = data else { return nil }
+                return value
+            }
+        
+        let blogError = blogResult
+            .compactMap { data -> String? in
+                guard case .failure(let error) = data else { return nil }
+                return error.localizedDescription
+            }
+        
+        // convert to cellData the value received from network
+        let cellData = blogValue
+            .map { blog -> [BlogListCellData] in
+                return blog.documents
+                    .map { doc in
+                        let thumbnailURL = URL(string: doc.thumbnail ?? "")
+                        return BlogListCellData(
+                            thumbnailURL: thumbnailURL,
+                            name: doc.name,
+                            title: doc.title,
+                            datetime: doc.datetime
+                        )
+                    }
+            }
+        
+        // type out of selecting alertsheet when opting for FilterView
+        let sortedType = alertActionTapped
+            .filter {
+                switch $0 {
+                case .title, .datetime:
+                    return true
+                default:
+                    return false
+                }
+            }
+            .startWith(.title)
+        
+        // MainViewController -> ListView
+        Observable
+            .combineLatest(
+                sortedType,
+                cellData
+            ) { type, data -> [BlogListCellData] in
+                switch type {
+                case .title:
+                    return data.sorted { $0.title ?? "" < $1.title ?? "" }
+                case .datetime:
+                    return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+                default:
+                    return data
+                }
+            }
+            .bind(to: listView.cellData)
+            .disposed(by: disposeBag)
+        
         let alertSheetForSorting = listView.headerView.sortButtonTapped
             .map { _ -> Alert in
                 return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
             }
         
-        alertSheetForSorting
+        let alertForErrorMessage = blogError
+            .map { message -> Alert in
+                return (
+                    title: "Oh, my..",
+                    message: "An unexpected error occurred. Please try again in a moment. \(message)",
+                    actions: [.confirm],
+                    style: .alert
+                )
+            }
+        
+        Observable
+            .merge(
+                alertSheetForSorting,
+                alertForErrorMessage
+            )
             .asSignal(onErrorSignalWith: .empty())
             .flatMapLatest { alert -> Signal<AlertAction> in
                 let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
